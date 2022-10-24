7c756380b2c931d4e1ff04d6e509cd18b5770329
Timurlan <rendan777@gmail.com>
2022-10-24 11:28:57 +0700

    upload 24.10.22

---
+++ Methods/src/Main.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static int i = 0;
+    public static int max = 10;
+
+    public static void main(String[] args) {
+        iterate();
+    }
+
+
+    public static void iterate() {
+
+        if (i < max) {
+            i = i + 1;
+            System.out.println(i);
+            iterate();
+        }
+
+    }
+
+
+
+
+}
